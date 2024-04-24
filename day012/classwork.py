192f774de2cc574dec27eb8b7f85fd72d676e6ed
gio101010 <Varika700@mail.ru>
2024-04-24 22:31:29 +0400

    sixth commit

---
+++ day012/classwork.py
@@ -0,0 +1,22 @@
+for i in range(1,6+1): 
+    print(i) 
+
+for i in range(6,0,-1):
+     print(i)
+
+for i in range(2, 21, 2):
+
+      print(i)
+
+
+for i in range(1,15,2):
+     print(i) 
+
+
+sum = 0
+
+for i in range(1,6):
+
+      sum = sum + 1
+      print(sum) 
+
